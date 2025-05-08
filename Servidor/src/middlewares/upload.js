ee7d2f4871e6e37f6fcb323b69265f407f4b70c3
Juan Daniel López <juandaniel.lopezh@um.es>
2025-05-08 22:39:37 +0200

    Backend: Modificado tamaño de las imágenes subidas a Cloudinary

--- Servidor/src/middlewares/upload.js
+++ Servidor/src/middlewares/upload.js
@@ -6,21 +6,26 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: (req, file) => {
     let folder = 'others'
+    let transformation = [{ width: 500, height: 500, crop: 'limit' }]
 
     if (req.url.includes('/avatar')) {
       folder = 'avatars'
+      transformation = [{ width: 200, height: 200, crop: 'limit' }]
     } else if (req.url.includes('/itinerary-image')) {
       folder = 'itineraries'
+      transformation = [{ width: 1000, height: 700, crop: 'limit' }]
     } else if (req.url.includes('/event-image')) {
       folder = 'events'
+      transformation = [{ width: 500, height: 500, crop: 'limit' }]
     } else if (req.url.includes('/list-image')) {
       folder = 'lists'
+      transformation = [{ width: 1000, height: 700, crop: 'limit' }]
     }
 
     return {
-      folder: folder,
+      folder,
       allowed_formats: ['jpg', 'jpeg', 'png'],
-      transformation: [{ width: 500, height: 500, crop: 'limit' }],
+      transformation,
       public_id: file.originalname
     }
   }
